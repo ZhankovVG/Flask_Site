a4d431a3a603a236756908503840ab0c29686af6
Vitaliy <sprinter-100m@ukr.net>
2023-07-15 14:39:40 +0300

    not a big flask website

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,15 +15,36 @@ class Item(db.Model):
     price = db.Column(db.Integer, nullable=False) 
     isActive = db.Column(db.Boolean, default=True)
 
+    def __repr__(self):
+        return self.title
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Получилась ошибка'
+    else:
+        return render_template('create.html')
 
 
 if __name__ == "__main__":
